758b54be25024d8ee5fb5550e35afaec0c7d4d55
Aditi-Agrawal07 <07aditiagrawal@gmail.com>
2024-03-03 22:19:41 +0530

    intro section added

--- angular_project/src/app/apicalling.service.ts
+++ angular_project/src/app/apicalling.service.ts
@@ -16,4 +16,15 @@ export class ApicallingService {
   getProducts(): Observable<any> {
     return this.httpClient.get("http://mobile.kalyanam.online/api/market")
   }
+
+  downloadFile(fileId: string): void {
+    const url = `https://drive.google.com/uc?export=download&id=${fileId}`;
+    this.httpClient.get(url, { responseType: 'blob' }).subscribe((response: Blob) => {
+      const downloadLink = document.createElement('a');
+      downloadLink.href = window.URL.createObjectURL(new Blob([response]));
+      downloadLink.setAttribute('download', 'filename.apk'); // Set your desired filename here
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+    });
+  }
 }
--- angular_project/src/app/app.component.ts
+++ angular_project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { ApicallingService } from './apicalling.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,10 @@ import { CardComponent } from './card/card.component';
 })
 export class AppComponent {
   title = 'angular_project';
+  constructor(private ApiCalling: ApicallingService){}
+
+  downloadApk(): void {
+    const fileId = '1lSNtBZHHHrUMpIvPs8odIwugwE3hmBN_'; // Replace with your actual file ID
+    this.ApiCalling.downloadFile(fileId);
+  }
 }
